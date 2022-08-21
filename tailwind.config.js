cd392da64480b1d5354a651f9542f604dd603964
Elias Girma <elias.girma0@gmail.com>
2022-08-21 21:22:06 +0300

    translate design to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
+		colors: {
+			'blue-1': '#2B3945', // Dark Mode Elements
+			'blue-2': '#202C37', // Dark Mode Background
+			'blue-3': '#111517', //Light Mode Text
+			'gray-1': '#FAFAFA', // Light Mode Background
+			'gray-2': '#858585', // Light Mode Input
+			white: '#FFFFFF', //Dark Mode Text & Light Mode Elements
+		},
+		fontFamily: {
+			'nunito-sans': ['Nunito Sans', 'sans-serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
